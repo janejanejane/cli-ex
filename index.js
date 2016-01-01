d745a104077f021b95b0e43df0e4196bca116701
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-01-01 20:28:18 +1300

    Add example command-line tool

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env Node
+
+console.log( process.argv );
+
+
+var userArgs = process.argv.slice( 2 );
+
+var searchPattern = userArgs[ 0 ];
+
+var exec = require( 'child_process' ).exec;
+var child = exec( 'ls -a | grep' + searchPattern, function( err, stdout, stderr ) {
+    console.log( stdout );
+});
